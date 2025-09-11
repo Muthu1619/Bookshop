4ac89c7777dd07d431347daac1594bf22fac92e0
Muthu1619 <muthxxa16@gmail.com>
2025-09-11 19:44:12 +0530

    Update __init__.py

--- myshop/__init__.py
+++ myshop/__init__.py
@@ -2,10 +2,11 @@ from flask import render_template,Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///D:/Codes/Python/bookolx/eazyshop/shop.db'
-app.config['SECRET_KEY']='50271c5e58c01c24ca7b9288'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "fallback-secret")
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
